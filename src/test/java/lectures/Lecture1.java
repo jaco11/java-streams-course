d19c30d3b77a67de1722080d5cb5f42527f8db16
Jaco Gerber <jacog@discovery.co.za>
2020-04-28 08:14:05 +0200

    Completed Lecture 1

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -22,11 +22,33 @@ public class Lecture1 {
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
 
+    List<Person> youngPeople = Lists.newArrayList();
+    final int limit = 10;
+    int counter = 0;
+
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        youngPeople.add(person);
+        counter++;
+        if (counter == limit) {
+          break;
+        }
+      }
+    }
+
+    for (Person young : youngPeople) {
+      System.out.println(young);
+    }
+
+
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
+   MockData.getPeople().stream()
+        .filter(person -> person.getAge() <= 18)
+        .limit(10)
+        .collect(Collectors.toList())
+       .forEach(System.out::println);
   }
 }
