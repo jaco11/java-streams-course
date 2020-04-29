b772981e3c2d59a5ffddd499e202a202e17d7aa6
Jaco Gerber <jacog@discovery.co.za>
2020-04-29 05:26:27 +0200

    Complated Lecture 3. Find Max and Min numnbers from List

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -12,12 +12,28 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    // Finding a minimum number
+//    final Integer min = numbers.stream()
+//        .min((number1, number2) -> number1 > number2 ? 1 : -1)
+//        .get();
+//    System.out.println(min);
+//    assertThat(min).isEqualTo(1);
 
+    final Integer min = numbers.stream()
+        .min(Comparator.naturalOrder())
+        .get();
+    System.out.println(min);
+    assertThat(min).isEqualTo(1);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    final Integer max = numbers.stream()
+        .max(Comparator.naturalOrder())
+        .get();
+    assertThat(max).isEqualTo(100);
+    System.out.println(max);
   }
+
 }
