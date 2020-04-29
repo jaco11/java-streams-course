1b4e4683e25a39f36bba4ba11d54c470dfb46965
Jaco Gerber <jacog@discovery.co.za>
2020-04-29 08:15:31 +0200

    Completed Lecture10, reduce() function in streams

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -5,12 +5,19 @@ import com.google.common.collect.Lists;
 import java.util.Arrays;
 import org.junit.Test;
 
+/**
+ * Reduce Streams
+ */
+
 public class Lecture9 {
 
   @Test
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
-
+    final int sum = Arrays.stream(integers).reduce(0, (a, b) -> a + b);
+    System.out.println(sum);
+    final Integer sum2 = Arrays.stream(integers).reduce(0, Integer::sum);
+    System.out.println(sum2);
   }
 
 
