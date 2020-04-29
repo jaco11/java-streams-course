63b2edf589c713e3ffd302b189315f59a7c7880c
Jaco Gerber <jacog@discovery.co.za>
2020-04-29 06:46:54 +0200

    Completed Lecture 5 with Filter and Map operations

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -15,28 +15,66 @@ import org.junit.Test;
 
 public class Lecture5 {
 
+  Predicate<Car> carPredicate = car -> car.getPrice() < 10000;
+
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
 
+    final List<Car> carsFiltered = cars.stream()
+        .filter(carPredicate)
+        .collect(Collectors.toList());
+
+    carsFiltered.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
+    assertThat(carsFiltered).hasSize(57);
   }
 
+  /**
+   * Transform with stream.map
+   * @throws Exception
+   */
   @Test
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
 
+//    final List<PersonDTO> dtos = people.stream()
+//        .map(person -> {
+//          PersonDTO dto = new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+//          return dto;
+//        }).collect(Collectors.toList());
+
+//    final List<PersonDTO> dtos = people.stream()
+//        .map(person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+//        .collect(Collectors.toList());
+
+    final List<PersonDTO> dtos = people.stream()
+        .map(PersonDTO::map)
+        .collect(Collectors.toList());
+
+    dtos.forEach(System.out::println);
+    assertThat(dtos).hasSize(1000);
+    System.out.println(dtos.size());
   }
 
+  /**
+   * stream.mapToDouble
+   * @throws Exception
+   */
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
-
+    final double average = MockData.getCars().stream()
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
+    System.out.println(average);
   }
 
   @Test
   public void test() throws Exception {
-
+      MockData.getCars().forEach(System.out::println);
   }
 }
 
