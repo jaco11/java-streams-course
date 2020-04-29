4998da36c00e22c8958f98c2303ede1bb25d2d89
Jaco Gerber <jacog@discovery.co.za>
2020-04-29 05:51:18 +0200

    Removing duplicates from list with distinct and sets

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -11,15 +11,31 @@ import org.junit.Test;
 
 public class Lecture4 {
 
+  /** Removing duplicates in a list (Distinct)
+   *
+   * @throws Exception
+   */
   @Test
   public void distinct() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+    final List<Integer> distinctNumbers = numbers.stream()
+        .distinct()
+        .collect(Collectors.toList());
+    assertThat(distinctNumbers).isEqualTo(ImmutableList.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    assertThat(distinctNumbers).hasSize(9);
+    System.out.println(distinctNumbers);
   }
 
+  /**
+   * Removing duplicates in a list with Collectors. Collectors.toSet
+   * @throws Exception
+   */
   @Test
   public void distinctWithSet() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    Set<Integer> numbersSet = numbers.stream().collect(Collectors.toSet());
+    System.out.println(numbersSet);
+    assertThat(numbersSet).hasSize(9);
 
   }
 }
