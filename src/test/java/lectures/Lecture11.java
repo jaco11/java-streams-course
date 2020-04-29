b4445f663a99f8fa1c638123779cdf103622dd98
Jaco Gerber <jacog@discovery.co.za>
2020-04-29 08:54:26 +0200

    Completed Lecture 11. Joining Strings with Streams

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -11,12 +11,27 @@ public class Lecture11 {
   @Test
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    StringBuilder stringBuilder = new StringBuilder();
+    for (String name : names) {
+      if (!stringBuilder.toString().isEmpty()) {
+        stringBuilder.append(", ");
+      }
+      stringBuilder.append(name);
+    }
+    System.out.println(stringBuilder.toString());
 
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    final String joinStr = names.stream().collect(Collectors.joining());
+    System.out.println(joinStr);
+    final String joinWithDelimiter = names.stream().collect(Collectors.joining(","));
+    System.out.println(joinWithDelimiter);
+    final String joiningDelimiterUpperCase = names.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.joining("|"));
+    System.out.println(joiningDelimiterUpperCase);
   }
 }
